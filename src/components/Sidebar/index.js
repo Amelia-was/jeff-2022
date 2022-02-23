120e91f223b7d0db4daff7816457f7b71a307910
Deisel <74831846+Amelia-was@users.noreply.github.com>
2022-02-23 09:35:39 -0500

    about text

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,15 @@ import React from 'react';
 function Sidebar() {
     return (
         <header>
-            sidebarit's jeff
+            <img src={require('../../assets/imgs/jeff-headshot.jpeg')} alt='headshot of Jeff Hicks' />
+            <h1>Jeffrey Hicks</h1>
+            <h2>University of Toronto</h2>
+            <p>Hello! I am a SSHRC Postdoctoral Fellow at the University of Toronto from 2021-2023. My current research spans a range of public economics topics including business taxation, unemployment insurance, low-income supports, and income inequality. I focus on research that can help improve the design and implementation of taxation and social insurance systems to further both fairness and efficiency objectives. My current projects use a range of data sources including large-scale linked administrative datasets, web-scraped textual data, and spatial information.</p>
+            <ul>
+                <li>email</li>
+                <li>linkedin</li>
+                <li>uoft website</li>
+            </ul>
         </header>
     );
 };
