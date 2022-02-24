c2174e8cc8db4606261fff198d5c43a3ea46ad18
Deisel <74831846+Amelia-was@users.noreply.github.com>
2022-02-24 17:27:10 -0500

    gh pages

--- src/components/Paper/index.js
+++ src/components/Paper/index.js
@@ -17,7 +17,7 @@ function Paper({ title, paperLink, links, authors, journal, abstract, isComingSo
             <>
                 <div className='flex row align-baseline mt-1'>
                     <button type='button' className='btn mr-1' id='abstract-button' onClick={() => setIsCollapsed(!isCollapsed)}><span className='arrow'>{isCollapsed ? '▸' : '▾'}</span> Abstract</button>
-                    {links && links.map(link => <a href={link.url} target='_blank' rel='noreferrer' className='link mr-2'>{link.title}</a>)}
+                    {links && links.map(link => <a key={link.title} href={link.url} target='_blank' rel='noreferrer' className='link mr-2'>{link.title}</a>)}
                 </div>
                 {isCollapsed ? <></> : <div id='abstract' className='abstract-text'>{abstract}</div>}
             </>
