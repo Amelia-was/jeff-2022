372cbf006a52a5029c4c898efff2494c09beb40c
Deisel <74831846+Amelia-was@users.noreply.github.com>
2022-02-25 16:46:36 -0500

    mobile responsive

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,10 +5,10 @@ function Sidebar() {
     const [isMobileOpen, setIsMobileOpen] = useState(false);
 
     return (
-        <header className='flex column'>
+        <header className='flex row justify-end-lg justify-start-md justify-center-sm align-baseline align-c-start'>
             <img className='hide-mobile' src={require('../../assets/imgs/jeff-headshot.jpg')} alt='headshot of Jeff Hicks' />
             <h1 className='mt-2'>Jeffrey Hicks</h1>
-            <ul className='contact flex row align-baseline'>
+            <ul className='contact flex row-reverse align-baseline justify-end mt-2'>
                 <li className='hide-mobile'>
                     <a href='mailto:jeffrey.hicks@utoronto.ca' className='icon-link'>
                         <IoMail className='contact-icon' />
@@ -19,15 +19,15 @@ function Sidebar() {
                     </a>
                 </li>
                 <li>
-                    <h2 className='mt-2'>University of Toronto</h2>
+                    <h2>University of Toronto</h2>
                 </li>
             </ul>
 
-            <div className='flex row align-baseline justify-evenly w-100 mt-3'>
+            <div className='flex row align-baseline justify-center-sm w-100 mt-2'>
 
-                <h3 className='mobile-nav show-mobile' onClick={() => setIsMobileOpen(!isMobileOpen)}>{isMobileOpen ? '▸' : '▾'} About</h3>
+                <h3 className='mobile-nav show-mobile mr-2' onClick={() => setIsMobileOpen(!isMobileOpen)}>{isMobileOpen ? '▸' : '▾'} About</h3>
 
-                <a href='mailto:jeffrey.hicks@utoronto.ca' className='icon-link show-mobile'>
+                <a href='mailto:jeffrey.hicks@utoronto.ca' className='icon-link show-mobile mr-2'>
                     <h3>Email</h3>
                 </a>
                 <a href='https://scholar.google.com/citations?user=Msyi62IAAAAJ&hl=en' className='icon-link show-mobile' target='_blank' rel='noreferrer'>
