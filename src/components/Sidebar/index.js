608cc5bf3220a7938fb082069c7d919323b5a28a
Deisel <74831846+Amelia-was@users.noreply.github.com>
2022-02-25 09:33:08 -0500

    sidebar links/summary

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -7,10 +7,19 @@ function Sidebar() {
             <img className='headshot' src={require('../../assets/imgs/jeff-headshot.jpg')} alt='headshot of Jeff Hicks' />
             <h1 className='mt-2'>Jeffrey Hicks</h1>
             <h2 className='mt-2'>University of Toronto</h2>
-            <p className='about mt-2'>Hello! I am a SSHRC Postdoctoral Fellow at the University of Toronto from 2021-2023. My current research spans a range of public economics topics including business taxation, unemployment insurance, low-income supports, and income inequality. I focus on research that can help improve the design and implementation of taxation and social insurance systems to further both fairness and efficiency objectives. My current projects use a range of data sources including large-scale linked administrative datasets, web-scraped textual data, and spatial information.</p>
+            <p className='about mt-2'>
+                Hello! I am an SSHRC Postdoctoral Fellow at the University of Toronto from 2021-2023. My research agenda primarily focuses on the design and implementation of taxation and social insurance systems. My current projects use a range of data sources including large-scale linked administrative datasets, web-scraped textual data, and spatial information.
+            </p>
             <ul className='contact flex row mt-2'>
-                <li><IoMail className='contact-icon' /></li>
-                <li><IoSchool className='contact-icon' /></li>
+                <li>
+                    <a href='mailto:jeffrey.hicks@utoronto.ca' className='icon-link'>
+                        <IoMail className='contact-icon' />
+                    </a></li>
+                <li>
+                    <a href='https://scholar.google.com/citations?user=Msyi62IAAAAJ&hl=en' className='icon-link' target='_blank' rel='noreferrer'>
+                        <IoSchool className='contact-icon' />
+                    </a>
+                </li>
             </ul>
         </header>
     );
